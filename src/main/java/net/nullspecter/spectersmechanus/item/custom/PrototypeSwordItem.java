3f565b8451c061ad62956a602a875d11a8b45804
average-levi <bestgaming12354@gmail.com>
2024-02-14 17:39:13 +0000

    custom item class added no func

---
+++ src/main/java/net/nullspecter/spectersmechanus/item/custom/PrototypeSwordItem.java
@@ -0,0 +1,12 @@
+package net.nullspecter.spectersmechanus.item.custom;
+
+import net.minecraft.item.Item;
+
+public class PrototypeSwordItem extends Item {
+
+    public PrototypeSwordItem(Settings settings) {
+        super(settings);
+       
+    }
+    
+}
